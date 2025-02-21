7e7db9b2bf8570461aa2aac083c768e8eb0c0a90
AfroOrange <nuhacet1998@gmail.com>
2025-02-21 16:30:20 +0000

    sytle: update css for navbar

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,12 +1,3 @@
-// make an action for the exampleButton
-// when the button is clicked, the text of the button will change
-// to "I am clicked"
-// and the background color will change to red
-// and the text color will change to white
-// and the border color will change to red
-// and the border width will change to 2px
-
-
 // get the button element
 const exampleButton = document.getElementById('exampleButton');
 const mainText = document.getElementById('mainText');
